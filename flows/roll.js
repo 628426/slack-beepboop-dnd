fae62f1f182c6beb407ffc09cb388b6a05cc23aa
Nathan Rogers <628426@gmail.com>
2016-10-12 08:14:09 +1100

    Error handling

--- flows/roll.js
+++ flows/roll.js
@@ -4,18 +4,16 @@ const os = require('os')
 module.exports = (slapp) => {
 
     slapp.command('/roll', /.*/, (msg, text) => {
-        var rolltext = msg.body.text;
 
-        if (!msg.body || !msg.body.text) {
-            msg.respond(":sob Sorry, I didn't understand you.  Try something like 2d6 or d20 or 3d6+4.  See https://github.com/troygoode/node-roll for more advanced examples");
-        } else {
-            try {               
-    
-                msg.respond(new require('roll')().roll(msg.body.text).result)
-            } catch(e) {
-                msg.response(e);
+        try {
+            if (!msg.body || !msg.body.text) {
+                msg.respond(":sob Sorry, I didn't understand you.  Try something like 2d6 or d20 or 3d6+4.  See https://github.com/troygoode/node-roll for more advanced examples");
+            } else {
+                msg.respond(new require('roll')().roll(msg.body.text).result);
             }
-            
+        }
+        catch (e) {
+            msg.respond(e)
         }
     })
 }
