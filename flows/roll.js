879fe5a64f48ea2b31121c5a6f93ecac17391562
Nathan Rogers <628426@gmail.com>
2016-10-12 08:07:46 +1100

    bot.yml customisation

--- flows/roll.js
+++ flows/roll.js
@@ -9,7 +9,13 @@ module.exports = (slapp) => {
         if (!msg.body || !msg.body.text) {
             msg.respond(":sob Sorry, I didn't understand you.  Try something like 2d6 or d20 or 3d6+4.  See https://github.com/troygoode/node-roll for more advanced examples");
         } else {
-            msg.respond(new require('roll')().roll(msg.body.text).result)
+            try {               
+    
+                msg.respond(new require('roll')().roll(msg.body.text).result)
+            } catch(e) {
+                msg.response(e);
+            }
+            
         }
     })
 }
