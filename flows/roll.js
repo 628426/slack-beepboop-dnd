33f117860336f47a63ffb9eae46ea992cae0b8d8
Nathan Rogers <628426@gmail.com>
2016-10-18 08:29:33 +1100

    moved roll package local

--- flows/roll.js
+++ flows/roll.js
@@ -1,15 +1,18 @@
 'use strict'
 const os = require('os')
+const Roll = require('./roll/index.js')
+
 
 module.exports = (slapp) => {
 
     slapp.command('/roll', /.*/, (msg, text) => {
 
         try {
-            if (!msg.body || !msg.body.text) {
+            if (msg.body.text = '') {
                 msg.respond(":sob Sorry, I didn't understand you.  Try something like 2d6 or d20 or 3d6+4.  See https://github.com/troygoode/node-roll for more advanced examples");
             } else {
-                msg.respond(new require('roll')().roll(msg.body.text).result);
+                var r = new Roll().roll(msg.body.text)
+                msg.respond(r.result);
             }
         }
         catch (e) {
