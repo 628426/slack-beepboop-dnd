716074a29b6d09a527cbdaefeccbae8ed116f6e7
Nathan Rogers <628426@gmail.com>
2016-10-18 08:34:57 +1100

    Verbose loggin

--- flows/roll.js
+++ flows/roll.js
@@ -6,7 +6,8 @@ const Roll = require('./roll/index.js')
 module.exports = (slapp) => {
 
     slapp.command('/roll', /.*/, (msg, text) => {
-
+        console.log('Command ' + text + ' found' )
+        console.log(JSON.stringify(msg))
         try {
             if (msg.body.text == '') {
                 msg.respond(":sob Sorry, I didn't understand you.  Try something like 2d6 or d20 or 3d6+4.  See https://github.com/troygoode/node-roll for more advanced examples");
