0e34feba479e631ca488fc2b2c9765d527e1d65a
Nathan Rogers <628426@gmail.com>
2016-10-18 08:38:27 +1100

    More detailed loggin

--- flows/roll.js
+++ flows/roll.js
@@ -12,11 +12,15 @@ module.exports = (slapp) => {
             if (msg.body.text == '') {
                 msg.respond(":sob Sorry, I didn't understand you.  Try something like 2d6 or d20 or 3d6+4.  See https://github.com/troygoode/node-roll for more advanced examples");
             } else {
+
+                console.log('about to roll ' + text)
                 var r = new Roll().roll(msg.body.text)
+                console.log(JSON.stringify(r));
                 msg.respond(r.result)
             }
         }
         catch (e) {
+            console.log('exception ' + e)
             msg.respond(e)
         }
     })
