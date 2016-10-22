a029cb18a67903e0e6ef27422c5942cc4790cfb1
Nathan Rogers <628426@gmail.com>
2016-10-22 22:41:31 +1100

    Party command

---
+++ flows/party.js
@@ -0,0 +1,26 @@
+'use strict'
+const os = require('os')
+const Roll = require('./roll/index.js')
+
+module.exports = (slapp) => {
+
+    slapp.command('/party', /.*/, (msg, text) => {
+        let token = msg.meta.bot_token
+        let id = msg.body.event.item.ts
+        let channel = msg.body.event.item.channel
+        slapp.client.usergroups.list({token}, function(err, data) {
+            console.log(process.env.SLACK_TOKEN)
+            for(var usergroup in data) {
+                console.log(usergroup.name)
+                slapp.client.users.list({token, usergroup}, function(err, data) {
+                    slapp.client.users.info({token, data}, function(err, data){
+                        msg.say(data.name)
+                    })
+                })
+            }
+        })
+
+    }
+}
+
+
