ed9005c05c2168aa7ba0c29e4ac1d999f59665dc
Nathan Rogers <628426@gmail.com>
2016-10-22 23:08:35 +1100

    setdm command

--- flows/setDm.js
+++ flows/setDm.js
@@ -4,20 +4,26 @@ module.exports = (slapp) => {
 
     slapp.command('/setdm', /.*/, (msg, text) => {
         let user = msg.body["user_name"]
+        if(!text){
+            msg.respond(":sob: Sorry, I couldn't understand you, you need to tell me a players name to make dm")
+            return;
+        }
 
         store.get("DM", function(err, data) {
             if(err) {
-                msg.respond(":sob Sorry, " + err + " occurred")
+                msg.respond(":sob: Sorry, " + err + " occurred")
+                return;
             }
             var dm = data || "griswold"
 
             if(dm != user) {
-                msg.respond(":sob Sorry, @" + dm + " is currently the dm and is the only player allowed to user the /setdm command" )
+                msg.respond(":sob: Sorry, @" + dm + " is currently the dm and is the only player allowed to user the /setdm command" )
+                return;
             }
 
             store.set("DM", text, function(err) {
                 if(!err) {
-                    msg.say(text + " is now the dm, look out")
+                    msg.say("@" + text + " is now the dm, look out")
                 }
             }) 
         })
