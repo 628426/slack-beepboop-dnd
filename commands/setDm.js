2e2a4e0d6c41a09f32ae7575fd69380a1bb939bb
Nathan Rogers <628426@gmail.com>
2016-10-24 12:30:31 +1100

    Check for players

--- commands/setDm.js
+++ commands/setDm.js
@@ -5,21 +5,14 @@ const store = require('beepboop-persist')()
 module.exports = function (keyword, msg, text, say) {
     v.requiresParameters(msg, text, 'a players name, like, fug.  E.g. /setdm fug', 1, say, function () {
         v.mustBeDm(msg, keyword, function () {
-            v.mustBeUser(msg, keyword, text, say, function() {
-            store.set("DM", text, function (err) {
-                if (err) {
-                    return say(`:sob: Sorry, ${err} occurred`)
-                }
-                return say(`:smiling_imp: @${text} is now the dm, look out`)
+            v.mustBeUser(msg, keyword, text, say, function () {
+                store.set("DM", text, function (err) {
+                    if (err) {
+                        return say(`:sob: Sorry, ${err} occurred`)
+                    }
+                    return say(`:smiling_imp: @${text} is now the dm, look out`)
+                })
             })
-            })
-
         })
     })
-}
-
-
-
-
-
 }
