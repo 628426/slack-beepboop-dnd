48dd7a9226cb9e51f2a835bc67ee404cc6cc3c1c
Nathan Rogers <628426@gmail.com>
2016-10-25 21:52:28 +1100

    Support for hosted db

--- commands/persist.js
+++ commands/persist.js
@@ -33,7 +33,7 @@ module.exports = function (slack, opts) {
 
     persistance.set = function (token, key, value, cb) {
         if (!this.channelCreated) {
-            checkOrCreateChannel(slack, token, function (err) {
+            checkOrCreateChannel(slack, token, opts.channel, function (err) {
                 if(err) return cb(err)
                 this.channelCreated = true;
                 setData(slack, token, opts.schema, opts.channel, key, function (err, savedValue) {
