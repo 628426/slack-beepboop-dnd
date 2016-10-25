e07a05e53ab870ac2730f30813faa7f4652d9d11
Nathan Rogers <628426@gmail.com>
2016-10-25 22:01:32 +1100

    Support for hosted db

--- commands/persist.js
+++ commands/persist.js
@@ -1,5 +1,5 @@
 function checkOrCreateChannel(slack, token, channel, cb) {
-    slack.channels.info({ token: token }, function (err, channel) {
+    slack.channels.info({ token: token, channel: channel }, function (err, channel) {
         if (err && err == 'channel_not_found') {
             slack.channels.create({ token: token }, function (err, channel) {
                 if (err) return cb(err)
