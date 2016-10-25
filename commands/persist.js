ba68136d7938c84120b7ddf31b09a89d25d9e192
Nathan Rogers <628426@gmail.com>
2016-10-25 22:13:05 +1100

    Support for hosted db

--- commands/persist.js
+++ commands/persist.js
@@ -1,8 +1,8 @@
 function checkOrCreateChannel(slack, token, channel, cb) {
     slack.channels.info({ token: token, channel: channel }, function (err, channel) {
         if (err && err == 'channel_not_found') {
-            slack.channels.create({ token: token, channel: channel }, function (err, channel) {
-                if (err) return cb('channels.create::' + err)
+            slack.channels.create({ token: token, channel: channel }, function (errr, channel) {
+                if (errr) return cb('channels.create::' + errr)
                 return cb()
             })
         } else if (err) {
@@ -18,7 +18,7 @@ function setData(slack, token, schema, channel, key, value, cb) {
     slack.files.upload({ token: token, content: JSON.stringify(value, null, 4), filetype: 'json', filename: schema + '.' + key + '.json' }, function (err, file) {
         if (err) return cb('files.upload::' + err)
         value.updated = file.updated
-        cb(null, value)
+        return cb(null, value)
     })
 }
 
@@ -37,7 +37,7 @@ module.exports = function (slack, opts) {
     persistance.set = function (key, value, cb) {
         if (!this.channelCreated) {
             checkOrCreateChannel(slack, opts.token, opts.channel, function (err) {
-                if(err) return cb('checkOrCreateChannel::' + err)
+                if (err) return cb('checkOrCreateChannel::' + err)
                 this.channelCreated = true;
                 setData(slack, opts.token, opts.schema, opts.channel, key, function (err, savedValue) {
                     if (err) return cb('setData::' + err)
