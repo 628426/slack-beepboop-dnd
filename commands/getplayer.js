62b60e875a0e712e7e724aaff0142b4b93453786
Nathan Rogers <628426@gmail.com>
2016-10-29 20:29:38 +1100

    Added normalisation of names

--- commands/getplayer.js
+++ commands/getplayer.js
@@ -3,7 +3,7 @@ const v = require('./validation')
 module.exports = function (keyword, msg, text, say) {
     let db = require('./db')(require('./persist')(msg._slapp.client, { token: msg.meta.app_token, schema: 'dnd' }))
     v.requiresParameters(msg, text, 'a players name, like, fug.  E.g. /setplayer fug hp 99', 1, say, function () {
-        v.mustBeUser(msg, keyword, text, say, function () {
+        v.mustBeUser(msg, keyword, text.split(' ')[0], say, function () {
 
             db.getPlayer(text.split(' ')[0], function (err, player) {
                 let args = text.split(' ')
