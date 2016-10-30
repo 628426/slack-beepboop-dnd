2dd0e3d101bbde4457a02d90fc496504566b475c
Nathan Rogers <628426@gmail.com>
2016-10-30 22:02:36 +1100

    Added get

--- commands/getplayer.js
+++ commands/getplayer.js
@@ -1,6 +1,6 @@
 const v = require('./validation')
 
-module.exports = function (keyword, msg, args, say) {
+module.exports = function (msg, args, say) {
     let db = require('./db')(require('./persist')(msg._slapp.client, { token: msg.meta.app_token, schema: 'dnd' }))
     v.requiresParameters(msg, args.join(' '), 'a players name, like, fug.  E.g. /set player fug hp 99', 1, say, function () {
         v.mustBeUser(msg, keyword, args[0], say, function () {
--- commands/setDm.js
+++ commands/setDm.js
@@ -2,7 +2,7 @@ const v = require('./validation')
 const store = require('beepboop-persist')()
 
 
-module.exports = function (keyword, msg, args, say) {
+module.exports = function (msg, args, say) {
     v.requiresParameters(msg, args.join(' '), 'a players name, like, fug.  E.g. /setdm fug', 1, say, function () {
         v.mustBeDm(msg, keyword, say, function () {
             v.mustBeUser(msg, keyword, args[0], say, function () {
