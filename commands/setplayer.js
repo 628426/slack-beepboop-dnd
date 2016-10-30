50ad14afa4ab7dbf74e3544b061decde0f17a0f5
Nathan Rogers <628426@gmail.com>
2016-10-31 05:55:07 +1100

    Fixe set player

--- commands/setplayer.js
+++ commands/setplayer.js
@@ -5,7 +5,7 @@ module.exports = function (msg, args, say) {
     let db = require('./db')(require('./persist')(msg._slapp.client, { token: msg.meta.app_token, schema: 'dnd' }))
 
     v.requiresParameters(msg, args.join(' '), 'a players name, like, fug.  E.g. /set player fug hp 99', 1, say, function () {
-        v.mustBeDm(msg, keyword, say, function () {
+        v.mustBeDm(msg, say, function () {
             v.mustBeUser(msg, args[0], say, function () {
                 if (args.length == 1) {
                     return say(`:sob: Right, I couldn't quite understand you, you'll have to be clearer. I know you want to do something to poor @${args[0]} but I'm not sure what. Try something like /set player ${args[0]} hp 99`)
