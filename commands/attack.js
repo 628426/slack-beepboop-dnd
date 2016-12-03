b4fe8300ab3fb7ec509d0a1a5ba27013f438b3f6
628426 <628426@gmail.com>
2016-12-03 21:08:18 +1100

    Weapon attack and damage roll support

--- commands/attack.js
+++ commands/attack.js
@@ -50,35 +50,38 @@ module.exports.attack = function (msg, params, say) {
     let user = msg.body["user_name"].toLowerCase()
 
     db.getPlayer(user, function (err, player) {
-        console.log('got player a ' + user)
-        console.log('got playerj a ' + JSON.stringify(player))
-        let weapon = getWeapon(player, params)
-        console.log('got playerj ab ' + JSON.stringify(player))
-        console.log('got weapon ' + weapon.name)
+        if (err) throw err
+        if (player) {
+            console.log('got player a ' + user)
+            console.log('got playerj a ' + JSON.stringify(player))
+            let weapon = getWeapon(player, params)
+            console.log('got playerj ab ' + JSON.stringify(player))
+            console.log('got weapon ' + weapon.name)
 
-        rollText = `${weapon.attack}+${mechanics.getProficiency(player)}+${mechanics.getModifier(player, weapon.attackmodifier)}`
-        console.log('got rolltext ' + rollText)
-        console.log('got playerj ac ' + JSON.stringify(player))
-        let rollDesc = `${weapon.attack}+${mechanics.getProficiency(player)}(player proficiency bonus for level ${player.level}+${mechanics.getModifier(player, weapon.attackmodifier)}`
-        console.log('got rollDesc ' + rollDesc)
-        let flavour = ``
-        let roll = new r().roll(rollText)
-        if (params && params.length > 0) {
-            flavour = ` ${params.join(' ')}`
-        }
-        console.log('got flavour ' + flavour)
-        let result = `@${user} made an attack roll (${weapon.name}${flavour}) of  _${rollText}_ for and got ${roll.result} (dice rolled were ${JSON.stringify(roll.rolled)})`
+            rollText = `${weapon.attack}+${mechanics.getProficiency(player)}+${mechanics.getModifier(player, weapon.attackmodifier)}`
+            console.log('got rolltext ' + rollText)
+            console.log('got playerj ac ' + JSON.stringify(player))
+            let rollDesc = `${weapon.attack}+${mechanics.getProficiency(player)}(player proficiency bonus for level ${player.level}+${mechanics.getModifier(player, weapon.attackmodifier)}`
+            console.log('got rollDesc ' + rollDesc)
+            let flavour = ``
+            let roll = new r().roll(rollText)
+            if (params && params.length > 0) {
+                flavour = ` ${params.join(' ')}`
+            }
+            console.log('got flavour ' + flavour)
+            let result = `@${user} made an attack roll (${weapon.name}${flavour}) of  _${rollText}_ for and got ${roll.result} (dice rolled were ${JSON.stringify(roll.rolled)})`
 
-        console.log('get resuilt' + result)
-        if (weapon.name != player.lastWeaponAttackedWith) {
-            console.log('writing back')
-            db.setPlayer(user, user, 'set', ['lastWeaponAttackedWith', weapon.name], Date.now(), function (err, settedplayer) {
-                console.log('writing returning')
+            console.log('get resuilt' + result)
+            if (weapon.name != player.lastWeaponAttackedWith) {
+                console.log('writing back')
+                db.setPlayer(user, user, 'set', ['lastWeaponAttackedWith', weapon.name], Date.now(), function (err, settedplayer) {
+                    console.log('writing returning')
+                    return say(result)
+                })
+            } else {
+                console.log('returning no write')
                 return say(result)
-            })
-        } else {
-            console.log('returning no write')
-            return say(result)
+            }
         }
 
     })
@@ -89,20 +92,22 @@ module.exports.damage = function (msg, params, say) {
     let user = msg.body["user_name"]
 
     db.getPlayer(user, function (err, player) {
-        let weapon = getWeapon(player, [player.lastWeaponAttackedWith])
+        if (err) throw err
+        if (player) {
+            let weapon = getWeapon(player, [player.lastWeaponAttackedWith])
 
-        let mechanics = require('./mechanics.js')
-        rollText = `${weapon.damage}+${mechanics.getModifier(player, weapon.attackmodifier)}`
+            let mechanics = require('./mechanics.js')
+            rollText = `${weapon.damage}+${mechanics.getModifier(player, weapon.attackmodifier)}`
 
-        let flavour = ``
-        let roll = new r().roll(rollText)
-        if (params && params.length > 0) {
-            flavour = ` ${params.join(' ')}`
+            let flavour = ``
+            let roll = new r().roll(rollText)
+            if (params && params.length > 0) {
+                flavour = ` ${params.join(' ')}`
+            }
+            let result = `@${user} made an damage roll (${weapon.name}${flavour}) of  _${rs}_ for and got ${roll.result} (dice rolled were ${JSON.stringify(roll.rolled)})`
+
+            return say(result)
+
         }
-        let result = `@${user} made an damage roll (${weapon.name}${flavour}) of  _${rs}_ for and got ${roll.result} (dice rolled were ${JSON.stringify(roll.rolled)})`
-
-        return say(result)
-
-
     })
 }
