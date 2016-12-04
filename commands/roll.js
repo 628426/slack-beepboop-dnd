2e8dcced8e2e5e27560c298bf4753a02144faf6a
628426 <628426@gmail.com>
2016-12-04 20:30:36 +1100

    Support roll initiative

--- commands/roll.js
+++ commands/roll.js
@@ -21,21 +21,20 @@ module.exports = function (msg, params, say) {
             } else if (params[0].toLowerCase() == "damage") {
                 console.log('calling attack.damage')
                 return require('./attack.js').damage(msg, params.slice(1), say)
+            }
+            let commands = require('./globals.js').commands
+            if (commands['check'] &&
+                commands['check'][params[0]]) {
+                return commands['check'][params[0]].cmd(msg, params, say)
             } else {
-                let commands = require('./globals.js').commands
-                if (commands['check'] &&
-                    commands['check'][params[0]]) {
-                    return commands['check'][params[0]].cmd(msg, params, say)
-                } else {
-                    let reason = ''
-                    if (params.length > 1) {
-                        reason = ' (_' + params.slice(1).join(' ') + '_)'
-                    }
-
-                    var roll = new r().roll(params[0])
-
-                    return say(`@${msg.body["user_name"]} rolled _${params[0]}_${reason} and got ${roll.result} (dice rolled were ${JSON.stringify(roll.rolled)})`)
+                let reason = ''
+                if (params.length > 1) {
+                    reason = ' (_' + params.slice(1).join(' ') + '_)'
                 }
+
+                var roll = new r().roll(params[0])
+
+                return say(`@${msg.body["user_name"]} rolled _${params[0]}_${reason} and got ${roll.result} (dice rolled were ${JSON.stringify(roll.rolled)})`)
             }
         })
 }
